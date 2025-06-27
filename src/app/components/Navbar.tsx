e001db9ac635e0ef3275032a667cf37beff1b464
Artistkent <collins.ose5@gmail.com>
2025-06-27 18:12:36 +0100

    navbar changes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Image from 'next/image'
 import { FaBars } from 'react-icons/fa';
 import { FaTimes } from 'react-icons/fa';
 import { motion, AnimatePresence } from 'framer-motion';
@@ -15,13 +16,33 @@ const navLinks = [
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+  const handleScroll = () => {
+    const navbar = document.querySelector('nav');
+    const navbarHeight = navbar?.clientHeight || 0;
+    setScrolled(window.scrollY > (navbarHeight/3)); // Adjust threshold as needed
+  };
+
+  window.addEventListener('scroll', handleScroll);
+  return () => window.removeEventListener('scroll', handleScroll);
+}, []);
 
   return (
-    <nav className="sticky top-0 z-50 bg-white/10 backdrop-blur border-b border-white/20">
+    <nav id='nav' className="sticky top-0 z-50 bg-white/10 backdrop-blur border-b border-white/20">
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
         {/* Brand */}
-        <a href="#" className="text-xl font-bold text-white tracking-wide">
+        <a href="#" className="flex flex-col items-center font-bold bg-gradient-to-r from-[#e1b353] to-[#7b7e3a] bg-clip-text text-transparent tracking-wide text-xs md:text-sm drop-shadow-lg hover:drop-shadow-xl transition-transform duration-300 transform hover:scale-110">
+             <Image
+      src="/EdLogoEdit.png"
+      width={50}
+      height={10}
+      alt="logo"
+      className='drop-shadow-lg hover:drop-shadow-xl transition-transform duration-300 transform hover:scale-110'
+    />
           Edyssey
+         
         </a>
 
         {/* Desktop Links */}
@@ -31,7 +52,7 @@ export default function Navbar() {
             <a
               key={link.href}
               href={link.href}
-              className="px-4 py-1.5 rounded-2xl bg-secondary hover:bg-secondary-500 text-white/80 hover:text-white transition text-sm font-medium"
+              className={`px-4 py-1.5 rounded-2xl hover:text-white transition-colors duration-500 transform text-sm font-bold  ${scrolled ? 'bg-primary text-black hover:bg-primary/70' : 'bg-secondary hover:bg-secondary-500 text-white hover:text-white/80'}`}
             >
               {link.label}
             </a>
